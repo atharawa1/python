c48e3a534f54f2b9b406dd9e364d59157f550ec8
atharawa1 <atharawa@eng.rizvi.edu.in>
2025-04-24 08:11:45 +0530

    Add files via upload

---
+++ PRACTICE CODE/1.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int value;
+    printf("Enter the Value=");
+    scanf("%d",value);
+    switch(value)
+    {
+        case 1:
+        printf("Value is 1");
+        break;
+        case 2:
+        printf("Value is 2");
+        break;
+                case 3:
+        printf("Value is 3");
+        break;
+                case 4:
+        printf("Value is 4");
+        break;
+                case 5:
+        printf("Value is 5");
+        break;
+        case 6:
+        printf("Value is 6");
+        break;
+                case 7:
+        printf("Value is 7");
+        break;
+                case 8:
+        printf("Value is 8");
+        break;
+                case 9:
+        printf("Value is 9");
+        break;
+                case 10:
+        printf("Value is 10");
+        break;
+                default:
+        printf("OUT OF RANGE");
+        break;
+    }
+    return 0;
+}
